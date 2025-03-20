02059fa2f38a1802255ccf6e891ae548bd358658
Anantha Chary C M <ananthacm2005@gmail.com>
2025-03-20 18:19:46 +0000

    Updated

--- http-server/index.js
+++ http-server/index.js
@@ -1,17 +1,60 @@
 const http = require("http");
 const fs = require("fs");
-fs.readFile("home.html", (err, home) => {
-    console.log(home.toString());
-});
-fs.readFile("home.html", (err, home) => {
-    if (err) {
-        throw err;
-    }
-    http
-        .createServer((request, response) => {
-            response.writeHeader(200, { "Content-Type": "text/html" });
-            response.write(home);
+const minimist = require("minimist");
+
+// Parse command-line arguments
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000; // Default to port 3000 if --port is not provided
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+// Function to read files before starting the server
+function loadFile(filename) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, "utf-8", (err, data) => {
+            if (err) {
+                console.error(`Error reading ${filename}:`, err);
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+// Load all files and start the server
+Promise.all([
+    loadFile("home.html"),
+    loadFile("project.html"),
+    loadFile("registration.html")
+])
+    .then(([home, project, registration]) => {
+        homeContent = home;
+        projectContent = project;
+        registrationContent = registration;
+
+        // Create and start the server
+        http.createServer((request, response) => {
+            response.writeHead(200, { "Content-Type": "text/html" });
+
+            switch (request.url) {
+                case "/project.html":
+                    response.write(projectContent);
+                    break;
+                case "/registration.html":
+                    response.write(registrationContent);
+                    break;
+                default:
+                    response.write(homeContent);
+                    break;
+            }
             response.end();
-        })
-        .listen(3000);
-});
+        }).listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch(() => {
+        console.error("Server failed to start due to file loading error.");
+    });
